0bab5dba8aea08d2de39063f4c539c0324784174
Lucas <77316912+Hiki-otaku@users.noreply.github.com>
2024-08-12 00:23:39 +0700

    Update Kill_switch_for_Cloudflare_WARP.bat

--- Kill_switch_for_Cloudflare_WARP.bat
+++ Kill_switch_for_Cloudflare_WARP.bat
@@ -35,27 +35,24 @@ if not exist "%warp-loc%" (
 REM Main menu // Menu chính
 :menu
 cls
-echo Please disable WARP to proceed!
-echo Reboot the device if there's no network connectivity after re-enabling WARP!
-echo Kill Switch:
 REM status check // kiểm tra trạng thái
 netsh advfirewall firewall show rule name="%ruleSvc%" dir=out | find /I "%ruleSvc%" >nul
 if %errorlevel%==0 (
-    echo Status: active
+    set killSwitchStatus=Active
 ) else (
-    echo Status: disabled
+    set killSwitchStatus=Disabled
 )
-REM List // danh sách
-echo Menu:
-echo 1. Turn On
-echo 2. Turn Off
-echo 3. Check Connection
-echo 4. Exit
-
+REM MENU  // tuỳ chọn
+echo Kill Switch (Status: %killSwitchStatus%)
+echo. & echo. Options:
+echo    1. Enable
+echo    2. Disable
+echo    3. Ping Test
+echo    4. Exit
+echo.
 REM To give options // đưa ra lựa chọn
-choice /c 1234 /n /m "Please enter your selection: "
+choice /c 1234 /n /m "Enter your choice: "
 set "choice=%errorlevel%"
-
 cls
 REM To execute a choice // thực thi lựa chọn
 if "%choice%"=="1" goto on
@@ -65,19 +62,17 @@ if "%choice%"=="4" goto exit
 
 :on
 echo You have chosen TURN ON.
-
 cls
-REM List // danh sách
-echo Kill Switch model:
-echo 1. Maximum security (WARP Client cannot be updated)
-echo 2. High security
-echo 3. Standard security
-echo 4. Back to menu
-
+echo Kill Switch Modes
+echo. & echo  Please select a security mode: &echo.
+echo   1. Maximum Security (WARP updates off)
+echo   2. High Security
+echo   3. Standard Security
+echo   4. Return to main menu
+echo.
 REM To give options // đưa ra lựa chọn
-choice /c 1234 /n /m "Please enter your selection: "
+choice /c 1234 /n /m "Enter your choice: "
 set "choiceModel=%errorlevel%"
-
 cls
 REM To execute a choice // thực thi lựa chọn
 if "%choiceModel%"=="1" goto onMaximum
@@ -86,145 +81,127 @@ if "%choiceModel%"=="3" goto onStandard
 if "%choiceModel%"=="4" goto menu
 
 :onMaximum
+echo You have selected:/menu/enable/security_mode/maximum
 REM Check the firewall to see if there is an outbound rule named 'Cloudflare WARP'. // Kiểm tra tường lửa xem có luật đầu ra nào tên "Cloudflare WARP" đã tồn tại chưa.
 netsh advfirewall firewall show rule name="%ruleSvc%" dir=out | find /I "%ruleSvc%" >nul
 if %errorlevel%==0 (
-    echo Outbound Rules have been created. Moving on to the next step...
+    echo Outbound Rules name="%ruleSvc%" have been created. Moving on to the next step...
 ) else (
-    echo Creating Outbound Rules...
+    echo Outbound Rules name="%ruleSvc%" have been successfully created.
     netsh advfirewall firewall add rule name="%ruleSvc%" dir=out action=%action% program="%warp-svc%" enable=yes profile=%profile%
-    echo Outbound Rules have been successfully created. Moving on to the next step.
 )
-
 REM Set inbound and outbound connections to Block for Private profile. // Đặt kết nối đầu vào và đầu ra thành Chặn cho cấu hình mạng Riêng tư.
-netsh advfirewall set domainprofile firewallpolicy blockinbound,blockoutbound
 echo Domain profile has been set to block inbound and outbound connections.
-
-netsh advfirewall set privateprofile firewallpolicy blockinbound,blockoutbound
+netsh advfirewall set domainprofile firewallpolicy blockinbound,blockoutbound
 echo Private profile has been set to block inbound and outbound connections.
-
+netsh advfirewall set privateprofile firewallpolicy blockinbound,blockoutbound
 REM Set the network profile type to Private. // Cài đặt loại cấu hình mạng thành Riêng tư.
-powershell -Command "Set-NetConnectionProfile -InterfaceAlias '*' -NetworkCategory Private"
 echo Network profile type has been set to Private.
-echo Please turn WARP back on after about 10 seconds!
+powershell -Command "Set-NetConnectionProfile -InterfaceAlias '*' -NetworkCategory Private"
+echo. & echo PLEASE TURN WARP BACK ON AFTER ABOUT 5 SECONDS!
 timeout /t 5 /nobreak
 goto menu
 
 :onHigh
+echo You have selected:/menu/enable/security_mode/high
 REM Check the firewall to see if there is an outbound rule named 'Cloudflare WARP'. // Kiểm tra tường lửa xem có luật đầu ra nào tên "Cloudflare WARP" đã tồn tại chưa.
 netsh advfirewall firewall show rule name="%ruleSvc%" dir=out | find /I "%ruleSvc%" >nul
 if %errorlevel%==0 (
-    echo Outbound Rules have been created. Moving on to the next step...
+    echo Outbound Rules name="%ruleSvc%" have been created. Moving on to the next step...
 ) else (
-    echo Creating Outbound Rules...
+    echo Outbound Rules name="%ruleSvc%" have been successfully created.
     netsh advfirewall firewall add rule name="%ruleSvc%" dir=out action=%action% program="%warp-svc%" enable=yes profile=%profile%
-    echo Outbound Rules have been successfully created. Moving on to the next step.
 )
 netsh advfirewall firewall show rule name="%ruleMain%" dir=out | find /I "%ruleMain%" >nul
 if %errorlevel%==0 (
-    echo Outbound Rules have been created. Moving on to the next step...
+    echo Outbound Rules name="%ruleMain%" have been created. Moving on to the next step...
 ) else (
-    echo Creating Outbound Rules...
+    echo Outbound Rules name="%ruleMain%" have been successfully created.
     netsh advfirewall firewall add rule name="%ruleMain%" dir=out action=%action% program="%warp-main%" enable=yes profile=%profile%
-    echo Outbound Rules have been successfully created. Moving on to the next step.
 )
 REM Set inbound and outbound connections to Block for Private profile. // Đặt kết nối đầu vào và đầu ra thành Chặn cho cấu hình mạng Riêng tư.
-netsh advfirewall set domainprofile firewallpolicy blockinbound,blockoutbound
 echo Domain profile has been set to block inbound and outbound connections.
-
-netsh advfirewall set privateprofile firewallpolicy blockinbound,blockoutbound
+netsh advfirewall set domainprofile firewallpolicy blockinbound,blockoutbound
 echo Private profile has been set to block inbound and outbound connections.
-
+netsh advfirewall set privateprofile firewallpolicy blockinbound,blockoutbound
 REM Set the network profile type to Private. // Cài đặt loại cấu hình mạng thành Riêng tư.
-powershell -Command "Set-NetConnectionProfile -InterfaceAlias '*' -NetworkCategory Private"
 echo Network profile type has been set to Private.
-echo Please turn WARP back on after about 10 seconds!
+powershell -Command "Set-NetConnectionProfile -InterfaceAlias '*' -NetworkCategory Private"
+echo. & echo PLEASE TURN WARP BACK ON AFTER ABOUT 5 SECONDS!
 timeout /t 5 /nobreak
 goto menu
 
 :onStandard
+echo You have selected:/menu/enable/security_mode/standard
 REM Check the firewall to see if there is an outbound rule named 'Cloudflare WARP'. // Kiểm tra tường lửa xem có luật đầu ra nào tên "Cloudflare WARP" đã tồn tại chưa.
 netsh advfirewall firewall show rule name="%ruleMain%" dir=out | find /I "%ruleMain%" >nul
 if %errorlevel%==0 (
-    echo Outbound Rules have been created. Moving on to the next step...
+    echo Outbound Rules name="%ruleMain%" have been created. Moving on to the next step...
 ) else (
-    echo Creating Outbound Rules...
+    echo Outbound Rules name="%ruleMain%" have been successfully created.
     netsh advfirewall firewall add rule name="%ruleMain%" dir=out action=%action% program="%warp-main%" enable=yes profile=%profile%
-    echo Outbound Rules have been successfully created. Moving on to the next step.
 )
 netsh advfirewall firewall show rule name="%ruleCli%" dir=out | find /I "%ruleCli%" >nul
 if %errorlevel%==0 (
-    echo Outbound Rules have been created. Moving on to the next step...
+    echo Outbound Rules name="%ruleCli%"have been created. Moving on to the next step...
 ) else (
-    echo Creating Outbound Rules...
+    echo Outbound Rules name="%ruleCli%" have been successfully created.
     netsh advfirewall firewall add rule name="%ruleCli%" dir=out action=%action% program="%warp-cli%" enable=yes profile=%profile%
-    echo Outbound Rules have been successfully created. Moving on to the next step.
 )
 netsh advfirewall firewall show rule name="%ruleDex%" dir=out | find /I "%ruleDex%" >nul
 if %errorlevel%==0 (
-    echo Outbound Rules have been created. Moving on to the next step...
+    echo Outbound Rules name="%ruleDex%" have been created. Moving on to the next step...
 ) else (
-    echo Creating Outbound Rules...
+    echo Outbound Rules name="%ruleDex%" have been successfully created.
     netsh advfirewall firewall add rule name="%ruleDex%" dir=out action=%action% program="%warp-dexi%" enable=yes profile=%profile%
-    echo Outbound Rules have been successfully created. Moving on to the next step.
 )
 netsh advfirewall firewall show rule name="%ruleDiag%" dir=out | find /I "%ruleDiag%" >nul
 if %errorlevel%==0 (
-    echo Outbound Rules have been created. Moving on to the next step...
+    echo Outbound Rules name="%ruleDiag%" have been created. Moving on to the next step...
 ) else (
-    echo Creating Outbound Rules...
+    echo Outbound Rules name="%ruleDiag%" have been successfully created.
     netsh advfirewall firewall add rule name="%ruleDiag%" dir=out action=%action% program="%warp-diag%" enable=yes profile=%profile%
-    echo Outbound Rules have been successfully created. Moving on to the next step.
 )
 netsh advfirewall firewall show rule name="%ruleSvc%" dir=out | find /I "%ruleSvc%" >nul
 if %errorlevel%==0 (
-    echo Outbound Rules have been created. Moving on to the next step...
+    echo Outbound Rules name="%ruleSvc%" have been created. Moving on to the next step...
 ) else (
-    echo Creating Outbound Rules...
+    echo Outbound Rules name="%ruleSvc%" have been successfully created. Moving on to the next step.
     netsh advfirewall firewall add rule name="%ruleSvc%" dir=out action=%action% program="%warp-svc%" enable=yes profile=%profile%
-    echo Outbound Rules have been successfully created. Moving on to the next step.
 )
-
 REM Set inbound and outbound connections to Block for Private profile. // Đặt kết nối đầu vào và đầu ra thành Chặn cho cấu hình mạng Riêng tư.
-netsh advfirewall set domainprofile firewallpolicy blockinbound,blockoutbound
 echo Domain profile has been set to block inbound and outbound connections.
-
-netsh advfirewall set privateprofile firewallpolicy blockinbound,blockoutbound
+netsh advfirewall set domainprofile firewallpolicy blockinbound,blockoutbound
 echo Private profile has been set to block inbound and outbound connections.
-
+netsh advfirewall set privateprofile firewallpolicy blockinbound,blockoutbound
 REM Set the network profile type to Private. // Cài đặt loại cấu hình mạng thành Riêng tư.
-powershell -Command "Set-NetConnectionProfile -InterfaceAlias '*' -NetworkCategory Private"
 echo Network profile type has been set to Private.
-echo Please turn WARP back on after about 10 seconds!
+powershell -Command "Set-NetConnectionProfile -InterfaceAlias '*' -NetworkCategory Private"
+echo. & echo PLEASE TURN WARP BACK ON AFTER ABOUT 5 SECONDS!
 timeout /t 5 /nobreak
 goto menu
 
 :off
-echo You have chosen TURN OFF.
+echo You have selected:/menu/disable
 echo Restore Windows to factory setting...
 powershell -Command "Set-NetConnectionProfile -InterfaceAlias '*' -NetworkCategory Public"
 echo Network profile type has been set to Public.
-
 netsh advfirewall set domainprofile firewallpolicy blockinbound,allowoutbound
-echo The domain profile has been configured to block incoming connections and allow outgoing ones.
-
+echo The domain profile has been configured to block inbound connections and allow outbound connections.
 netsh advfirewall set privateprofile firewallpolicy blockinbound,allowoutbound
-echo The Private profile has been configured to block incoming connections and allow outgoing ones.
-
-netsh advfirewall firewall delete rule name="Cloudflare WARP" Direction=out
+echo The Private profile has been configured to block inbound connections and allow outbound connections.
 netsh advfirewall firewall delete rule name="%ruleMain%" Direction=out
 netsh advfirewall firewall delete rule name="%ruleCli%" Direction=out
 netsh advfirewall firewall delete rule name="%ruleDex%" Direction=out
 netsh advfirewall firewall delete rule name="%ruleDiag%" Direction=out
 netsh advfirewall firewall delete rule name="%ruleSvc%" Direction=out
 echo The outbound rules has been removed.
-
 timeout /t 4
 goto menu
 
 :check
 @echo off
-start cmd /k ping 8.8.8.8 -t
+start "Checking connection to 8.8.8.8" cmd /k ping 8.8.8.8 -t
 goto menu
 
 :exit
